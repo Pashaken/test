52debcc59638a328788406e2f0bb262e94574310
Павел Соляных <pasha.sol788@yandex.ru>
2023-10-04 13:08:48 +0300

    Add variable laungles

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  
+  var lg := readinteger('Выберете язык/Select language/Valitse kieli : 1 - Русский , 2 - English , 3 - Suomalainen  ');
+  assert((lg >=1) and (lg <=3));
+  if lg = 1 then
+    print('Привет Мир!')
+  else if lg = 2 then
+    print('Hello World!')
+  else
+    print('Hei maailma!')
 end.
